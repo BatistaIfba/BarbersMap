3bb9c53934c1cc4bcaed1119d5e17126d26371ed
BatistaJnr <joaobatistajunior.029@gmail.com>
2025-10-31 20:42:31 -0300

    Criei o Menu logado para o barbeiro e o cliente

--- menu.py
+++ menu.py
@@ -11,3 +11,33 @@ def menu():
     print("5 - Sair")
     opc = int(input("Digite uma opção: "))
     return opc
+
+def menu_barbeiro():
+    print("=============================")
+    print("|        Barber´sMap        |")
+    print("|   Interface do Barbeiro   |")
+    print("=============================")
+    print("1 - Informações profissionais")
+    print("2 - Serviços")
+    print("3 - Agendamentos")
+    print("4 - Histórico")
+    print("5 - Receita")
+    print("6 - Suporte")
+    print("7 - Sair")
+    opc = int(input("Digite uma opção: "))
+    return opc
+
+def menu_cliente():
+    print("=============================")
+    print("|        Barber´sMap        |")
+    print("|   Serviços de barbeiros   |")
+    print("=============================")
+    print("1 - Buscar serviços disponiveis")
+    print("2 - Informações pessoais")
+    print("3 - Agendamentos")
+    print("4 - Histórico")
+    print("5 - Atendimento ao consumidor")
+    print("6 - Sair")
+    opc = int(input("Digite uma opção: "))
+    return opc
+
